c0172eeab77af35f5c164bb4fc5c85ba665abc7f
sangsoo <wsckm1@gmail.com>
2025-07-31 20:22:20 +0900

    chore: base url 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/front_6th_chapter1-3/' : '';
+const base = process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-1/' : '';
 
 export default defineConfig({
   base,
